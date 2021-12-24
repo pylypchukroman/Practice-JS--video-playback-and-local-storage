9c61a15448330be048c900e14eabe35ec8cfa690
pylypchukroman <romanpylypchuk@gmail.com>
2021-12-24 15:15:37 +0200

    додає рішення задачі 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const USAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+
+const savedData = localStorage.getItem(USAGE_KEY);
+testPreviousSavings();
+
+form.addEventListener('input', throttle(onFormInput, 500));
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem(USAGE_KEY, JSON.stringify(formData));
+}
+
+function testPreviousSavings() {
+  if (JSON.parse(savedData)) {
+    console.log(JSON.parse(savedData));
+    input.value = JSON.parse(savedData).email;
+    textarea.value = JSON.parse(savedData).message;
+  }
+}
+
+form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(JSON.parse(savedData));
+  e.currentTarget.reset();
+  localStorage.removeItem(USAGE_KEY);
+}
--- src/js/index.js
+++
@@ -1,1 +0,0 @@
-const throttle = require('lodash.throttle');
